ade0bf4d4fb152864d563b08254bbebcaef6cc75
Arnav Mehta <77837038+ArnavMehta3000@users.noreply.github.com>
2024-04-10 23:50:54 +0100

    Update WarpWin.h

--- Warp/Common/WarpWin.h
+++ Warp/Common/WarpWin.h
@@ -1,11 +1,9 @@
 #pragma once
 
-#pragma warning(push)
-#pragma warning(disable : 4005)
 #define NOMINMAX
 #define NOMCX
 #define NOSERVICE
 #define NOHELP
-#pragma warning(pop)
+#define WIN32_LEAN_AND_MEAN
 
 #include <Windows.h>
