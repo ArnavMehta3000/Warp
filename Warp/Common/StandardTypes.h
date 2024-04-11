b2f8367801d509deb662b7459ecda091c9a70c3c
Arnav Mehta <arnavdatt@gmail.com>
2024-04-11 17:44:52 +0100

    Made standard types constexprs inline

--- Warp/Common/StandardTypes.h
+++ Warp/Common/StandardTypes.h
@@ -14,15 +14,15 @@ namespace Warp::Types
 	using u32 = uint32_t;
 	using u64 = uint64_t;
 
-    	constexpr u64 u64max = std::numeric_limits<u64>::max();
-	constexpr u32 u32max = std::numeric_limits<u32>::max();
-	constexpr u16 u16max = std::numeric_limits<u16>::max();
-	constexpr u8  u8max  = std::numeric_limits<u8>::max();
+    inline constexpr u64 u64max = std::numeric_limits<u64>::max();
+	inline constexpr u32 u32max = std::numeric_limits<u32>::max();
+	inline constexpr u16 u16max = std::numeric_limits<u16>::max();
+	inline constexpr u8  u8max  = std::numeric_limits<u8>::max();
 
-	constexpr i64 i64max = std::numeric_limits<i64>::max();
-	constexpr i32 i32max = std::numeric_limits<i32>::max();
-	constexpr i16 i16max = std::numeric_limits<i16>::max();
-	constexpr i8  i8max  = std::numeric_limits<i8>::max();
+	inline constexpr i64 i64max = std::numeric_limits<i64>::max();
+	inline constexpr i32 i32max = std::numeric_limits<i32>::max();
+	inline constexpr i16 i16max = std::numeric_limits<i16>::max();
+	inline constexpr i8  i8max  = std::numeric_limits<i8>::max();
 
 	using f32 = float;
 	using f64 = double;
