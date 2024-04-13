16011f5c480431c934b746641513ce9a1f156f06
Arnav Mehta <arnavdatt@gmail.com>
2024-04-13 02:07:30 +0100

    Update timer

--- Warp/Time/Timer.h
+++ Warp/Time/Timer.h
@@ -4,41 +4,46 @@
 
 namespace Warp
 {
+	template <typename Real = Types::f32>
 	class Timer
 	{
 		using ClockType = std::chrono::high_resolution_clock;
-		
-		Timer()
-			: m_startTime(ClockType::now())
-			, m_deltaTime(0.0f)
+	public:
+		Timer() : m_deltaTime(0.0f) { }
+
+		static Timer Create()
 		{
-			m_lastTime = m_startTime;
+			Timer t = Timer();
+			t.m_startTime = ClockType::now();
+			t.m_lastTime = t.m_startTime;
+			return t;
 		}
 
 		void Tick()
 		{
 			ClockType::time_point currentTime = ClockType::now();
-			m_deltaTime = std::chrono::duration<Types::f32>(currentTime - m_lastTime).count();
+			m_deltaTime = std::chrono::duration<Real>(currentTime - m_lastTime).count();
 			m_lastTime = currentTime;
 		}
 
-		inline Types::f32 GetDeltaTime() const
+		inline Real GetDeltaTime() const
 		{
 			return m_deltaTime;
 		}
 
-		inline Types::f32 GetElapsedTime() const
+		inline Real GetElapsedTime() const
 		{
-			return std::chrono::duration<Types::f32>(ClockType::now() - m_startTime).count();
+			return std::chrono::duration<Real>(ClockType::now() - m_startTime).count();
 		}
 
-		inline Types::f32 GetFPS() const
+		inline Real GetFPS() const
 		{
 			return 1.0f / m_deltaTime;
 		}
+
 	private:
 		ClockType::time_point m_startTime;
 		ClockType::time_point m_lastTime;
-		Types::f32            m_deltaTime;
+		Real            m_deltaTime;
 	};
 }
